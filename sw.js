c24c551f5e39afbeee7b1f2560e01027fe5d1bb0
ZHK1024 <389914070@qq.com>
2025-02-07 16:03:56 +0800

    更新站点

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20250207.1346';
+const cacheName = 'chirpy-20250207.1602';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
